640e8f4a2fb8c0cdf1cdac4632ccd0b1ae7794b9
Craig Beaman <Craig.Beaman@unb.ca>
2020-03-24 18:06:55 -0300

    Add files via upload

--- Division.java
+++ Division.java
@@ -2,5 +2,9 @@ package project;
 
 public class Division 
 {
+	private Register quotient = new Register();
+	private Register divisor = new Register();
+	private Register remainder = new Register();
+	
 	
 }
--- Multiplication.java
+++ Multiplication.java
@@ -2,5 +2,9 @@ package project;
 
 public class Multiplication 
 {
-
+	private Register muliplicand = new Register();
+	private Register muliplier = new Register();
+	private Register product = new Register();
+	
+	
 }
