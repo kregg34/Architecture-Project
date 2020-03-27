c2dbb6720f50776b97d6fa3e7942f0abbc427c6b
Elliot Kim <eric9601@gmail.com>
2020-03-27 19:03:01 -0300

    Update TrialDivision.java
    Using Long instead

--- TrialDivision.java
+++ TrialDivision.java
@@ -1,19 +1,18 @@
 import java.util.*;
 public class TrialDivision {
 
-	int input = 52;
-	int pid = 2;
-	List<Integer> factors = new ArrayList<Integer>();
+	long pid = 2;
+	List<Long> factors = new ArrayList<Long>();
 	Object[] factorsArr;
 	
-    Object[] doDivision(){
+    public Object[] doDivision(long input){
 	do {
 		if(input%pid == 0) {
 			factors.add(pid);
 			input = input/pid;
 		}
 		else {
-			pid = pid+1;
+			pid = pid++;
 		}
 	}while(input >= pid*pid);
 	factorsArr = factors.toArray();
