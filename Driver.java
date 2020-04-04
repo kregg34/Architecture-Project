76dadeb23bca745a2f1bd8696c61a9f99c67a54b
Craig Beaman <Craig.Beaman@unb.ca>
2020-04-04 14:45:39 -0300

    Update Driver.java

--- Driver.java
+++ Driver.java
@@ -7,8 +7,7 @@ public class Driver
 	public static void main(String [] args)
 	{
 		
-		BigInteger n2 = new BigInteger("66");
-		System.out.println("Test 2: Should be 2*3*5*5");
+		BigInteger n2 = new BigInteger("91");
 		
 		double startTime = System.currentTimeMillis();
 		System.out.println(TrialDivision.doDivision(n2).toString());
